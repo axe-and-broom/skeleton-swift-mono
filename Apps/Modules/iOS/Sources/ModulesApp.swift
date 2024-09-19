09e183b7744ca905f93bb16921ac3b1f0bc5050b
Patrik Nyblad <patrik.nyblad@easypark.net>
2024-09-19 23:35:02 +0200

    Create modules project base

---
+++ Apps/Modules/iOS/Sources/ModulesApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct ModulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
